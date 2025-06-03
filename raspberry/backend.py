5dbc276cbd804e0583408f123218b1bd296e9ef8
Anon Ymous <rbnelr@gmx.de>
2025-06-03 11:20:55 +0200

    show missing sample data as gaps in plot

--- raspberry/backend.py
+++ raspberry/backend.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import timestamps as ts
 from datetime import datetime, timedelta
 import database as db
+import time
 
 app = FastAPI()
 
@@ -15,8 +16,10 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
-def query_channel_range(channel_id, start, end):
+def query_channel_range(channel_id, start, end, gap_thres):
     with db.get_db_cursor() as cur:
+        t0 = time.perf_counter()
+        t0f = time.perf_counter()
         start_ts = ts.local_time_to_timestamp(start)
         end_ts = ts.local_time_to_timestamp(end)
 
@@ -25,20 +28,58 @@ def query_channel_range(channel_id, start, end):
                         where channel_id = %s and timestamp between %s and %s
                         """, (channel_id, start_ts, end_ts))
         results = cur.fetchall()
+        t1f = time.perf_counter()
+
+        t0p = time.perf_counter()
+        timestamps = []
+        values = []
+
+        if len(results) > 0:
+            prev_row = results[0] # no gap on first row
+            for row in results:
+                # add None entries for gaps if we determine data is missing (so frontend can show gaps by not drawing lines)
+                if row[0] - prev_row[0] > gap_thres:
+                    timestamps.append(None)
+                    values.append(None)
+                
+                timestamps.append(row[0])
+                values.append(row[1])
+
+                prev_row = row
+
         # TODO: is this slow in python? optimize? how? numpy or directly in sql?
         # how slow is json conversion? If sql produces string, can we avoid expensive conversion?
-        return { 'timestamps': [row[0] for row in results], 'values': [row[1] for row in results] }
+        #res = { 'timestamps': [row[0] for row in results], 'values': [row[1] for row in results] }
+
+        res = { 'timestamps': timestamps, 'values': values }
+        t1p = time.perf_counter()
+        t1 = time.perf_counter()
+        
+        print(f"  fetch time: {(t1f - t0f)*1000:.2f} ms") # The bottleneck by far, takes a lot more time than iterating the data in python, so json conversion is likely fast as well
+        print(f"  processing time: {(t1p - t0p)*1000:.2f} ms")
+        print(f" total query time: {(t1 - t0)*1000:.2f} ms")
+
+        return res
 
 @app.get("/data")
 async def get_data():
+    t0 = time.perf_counter()
+
     power_id, power_by_minute_id = db.get_channels()
     # show all for now
     start = datetime.now() - timedelta(days=9999)
     end = datetime.now() + timedelta(days=9999)
 
-    return {
-        'power': query_channel_range(power_id, start, end),
-        'by_minute': query_channel_range(power_by_minute_id, start, end),
+    # assume missing data if 3 times more elapsed time than supposed sample rate
+    gap_thres_power = 1000 *3
+    gap_thres_by_minute = 1000*60 *3
+
+    res = {
+        'power': query_channel_range(power_id, start, end, gap_thres_power),
+        'by_minute': query_channel_range(power_by_minute_id, start, end, gap_thres_by_minute),
     }
 
+    t1 = time.perf_counter()
+    print(f"total time: {(t1 - t0)*1000:.2f} ms")
 
+    return res
